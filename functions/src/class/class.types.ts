70c59e6c12edb3fe98b309208988e8714e583237
Alejandro Caceres Prado <cacerespradoa@ryanair.com>
2020-05-29 15:55:00 +0100

    clean class model

--- functions/src/class/class.types.ts
+++ functions/src/class/class.types.ts
@@ -2,71 +2,59 @@ export interface skill {
   levelBonus: number;
 }
 
-export interface classSkillTree {
-  armor: {
-    levelUpSkillPoints: number;
-    noArmor: skill;
-    leather: skill;
-    hardenedLeather: skill;
-    chainMail: skill;
-    plate: skill;
-  },
-  weapons: {
-    levelUpSkillPoints: number;
-    edge: skill;
-    blunt: skill;
-    twoHands: skill;
-    throw: skill;
-    projectile: skill;
-    dualWeapons: skill;
-    pole: skill;
-  },
-  exterior: {
-    levelUpSkillPoints: number;
-    climb: skill;
-    mount: skill;
-    swim: skill;
-    tracking: skill;
-  },
-  survival: {
-    levelUpSkillPoints: number;
-    ambush: skill;
-    hide: skill;
-    lockPick: skill;
-    trapDismantle: skill;
-  },
-  magic: {
-    levelUpSkillPoints: number;
-    newMagicListChance: number;
-    readRunes: skill;
-    useObject: skill;
-    aimedSpell: skill;
-    baseSpellBonus: skill;
-  },
-  global: {
-    sight: skill;
-    health: {
-      levelBonus: number;
-      levelUpSkillPoints: number;
-    },
-    languages: {
-      levelUpSkillPoints: number;
-    },
-  },
-  secondary: {},
-}
-
 export interface ClassModel {
-  className: string
-  skills: classSkillTree,
-}
-
-export class Class implements ClassModel {
-  className!: string;
-  skills!: classSkillTree;
-
-  constructor(classToCreate: ClassModel) {
-    this.className = classToCreate.className;
-    this.skills = classToCreate.skills;
-  }
+  className: string;
+  skills: {
+    armor: {
+      levelUpSkillPoints: number;
+      noArmor: skill;
+      leather: skill;
+      hardenedLeather: skill;
+      chainMail: skill;
+      plate: skill;
+    };
+    weapons: {
+      levelUpSkillPoints: number;
+      edge: skill;
+      blunt: skill;
+      twoHands: skill;
+      throw: skill;
+      projectile: skill;
+      dualWeapons: skill;
+      pole: skill;
+    };
+    exterior: {
+      levelUpSkillPoints: number;
+      climb: skill;
+      mount: skill;
+      swim: skill;
+      tracking: skill;
+    };
+    survival: {
+      levelUpSkillPoints: number;
+      ambush: skill;
+      hide: skill;
+      lockPick: skill;
+      trapDismantle: skill;
+    };
+    magic: {
+      levelUpSkillPoints: number;
+      newMagicListChance: number;
+      readRunes: skill;
+      useObject: skill;
+      aimedSpell: skill;
+      baseSpellBonus: skill;
+    };
+    global: {
+      sight: skill;
+      health: {
+        levelBonus: number;
+        levelUpSkillPoints: number;
+      };
+      languages: {
+        levelUpSkillPoints: number;
+      };
+    };
+    secondary: {};
+  };
 }
